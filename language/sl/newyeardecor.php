0476b2a7a24470b82bfabd1bb066e20152139466
max-ima <96241406+max-ima@users.noreply.github.com>
2022-05-11 09:49:33 +0200

    Slovenian
    Slovenian translation

---
+++ language/sl/newyeardecor.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package       New Year Decor
+ * @copyright (c) 2018 Татьяна5
+ * @license       http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * Slovenian Translation - Marko K.(max, max-ima,...)
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'NYD_USER_SNOW'		=> 'Omogoči sneg',
+	'NYD_USER_GARLAND'	=> 'Omogoči girlando',
+	'NYD_USER_IMAGE'	=> 'Omogoči novoletno sliko',
+));
